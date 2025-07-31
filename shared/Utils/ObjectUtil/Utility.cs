7999e9293a92407adbe6bd14c1d137de710463da
Sarim7714 <73176099+Sarim7714@users.noreply.github.com>
2025-07-31 16:18:28 +0530

    feat: configured redis (#4)
    Co-authored-by: sarimBasic <sarim.hussain@basichomeloan.com>

---
+++ shared/Utils/ObjectUtil/Utility.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace Utils.ObjectUtil
+{
+    public class Utility
+    {
+        public static string SerializeData<T>(T data)
+        {
+            return JsonSerializer.Serialize(data);
+        }
+        public static T? DeserializeData<T>(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
