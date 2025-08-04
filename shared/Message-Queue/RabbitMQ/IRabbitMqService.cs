596f684881e7d80380ee6b6ce6cbdad3bf5558ba
Amitabh <32864393+MakeAnIque@users.noreply.github.com>
2025-08-04 15:18:38 +0530

    Ed 28 (#7)
    * feat(Repo): Monorepo Build System setup
    
    * update(build-system): environments added in build system
    
    * Ed 34 set up husky to enforce jira style commit messages (#3)
    
    * Keep calm and commit
    
    * update
    
    * Test Husky
    
    * update
    
    * fix:add commit msg standard format and code format
    
    * fix: husky configured for linting
    
    * fix: git based handling
    
    * fix: commit mesage prefix
    
    ---------
    
    Co-authored-by: sarimBasic <sarim.hussain@basichomeloan.com>
    
    * feat: configured redis (#4)
    
    Co-authored-by: sarimBasic <sarim.hussain@basichomeloan.com>
    
    * Ed 42 configure orm library with repository pattern in shared (#5)
    
    * feat: configured redis
    
    * feat: Configured ORM Library with Repository Pattern in Shared
    
    * fix: resolved files
    
    ---------
    
    Co-authored-by: sarimBasic <sarim.hussain@basichomeloan.com>
    
    * feat: Configured RabbitMq using an Abstraction Layer (#6)
    
    Co-authored-by: sarimBasic <sarim.hussain@basichomeloan.com>
    
    ---------
    
    Co-authored-by: sarimBasic <sarim.hussain@basichomeloan.com>
    Co-authored-by: Sarim7714 <73176099+Sarim7714@users.noreply.github.com>

---
+++ shared/Message-Queue/RabbitMQ/IRabbitMqService.cs
@@ -0,0 +1,11 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace MessageQueue;
+
+public interface IRabbitMqService
+{
+    void Publish<T>(string queueName, T message, bool durable = true);
+    void Consume<T>(string queueName, Action<T, BasicDeliverEventArgs, IModel> onMessageReceived);
+
+}
