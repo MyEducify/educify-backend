199f57e2c6e037b55174a79068f59adb73dfaa17
Amitabh <32864393+MakeAnIque@users.noreply.github.com>
2025-08-07 11:48:05 +0530

    feat: Configured GRPC for internal microservice call (#10) (#12) (#13) (#14)
    Co-authored-by: Sarim7714 <73176099+Sarim7714@users.noreply.github.com>
    Co-authored-by: sarimBasic <sarim.hussain@basichomeloan.com>

---
+++ services/AuthService/SomeService.cs
@@ -0,0 +1,20 @@
+﻿using Microservice.Communication.GRPC.Protos;
+
+namespace AuthService
+{
+    public class SomeService
+    {
+        private readonly UserService.UserServiceClient _userServiceClient;
+
+        public SomeService(UserService.UserServiceClient userServiceClient)
+        {
+            _userServiceClient = userServiceClient;
+        }
+
+        public async Task CallUserGrpc()
+        {
+            var response = await _userServiceClient.GetUserByIdAsync(new GetUserRequest { UserId = "123" });
+            Console.WriteLine($"User: {response.Email}");
+        }
+    }
+}
