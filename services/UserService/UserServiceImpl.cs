55ae1e7b475c09314ad8cc70138d073786feabe9
Amitabh <32864393+MakeAnIque@users.noreply.github.com>
2025-08-07 11:46:50 +0530

    feat: Configured GRPC for internal microservice call (#10) (#12)
    Co-authored-by: Sarim7714 <73176099+Sarim7714@users.noreply.github.com>
    Co-authored-by: sarimBasic <sarim.hussain@basichomeloan.com>

---
+++ services/UserService/UserServiceImpl.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Core;
+using Microservice.Communication.GRPC.Protos;
+
+namespace UserService
+{
+    public class UserServiceImpl : Microservice.Communication.GRPC.Protos.UserService.UserServiceBase
+    {
+        public override Task<UserResponse> GetUserById(GetUserRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new UserResponse
+            {
+                UserId = request.UserId,
+                Email = "test@example.com",
+                Username = "testuser"
+            });
+        }
+    }
+
+}
