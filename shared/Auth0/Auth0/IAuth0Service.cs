1a6d1a1dcaf1091bb9c098c0394f55ac3429a13d
Amitabh <32864393+MakeAnIque@users.noreply.github.com>
2025-09-02 13:25:30 +0530

    Stage (#22)
    * feat: Configured GRPC for internal microservice call (#10) (#12)
    
    Co-authored-by: Sarim7714 <73176099+Sarim7714@users.noreply.github.com>
    Co-authored-by: sarimBasic <sarim.hussain@basichomeloan.com>
    
    * feat: Configured GRPC for internal microservice call (#10) (#12) (#13)
    
    Co-authored-by: Sarim7714 <73176099+Sarim7714@users.noreply.github.com>
    Co-authored-by: sarimBasic <sarim.hussain@basichomeloan.com>
    
    * Feat:Impliment Auth and add docker file to build through nuke
    
    * Feat:Impliment Auth and add docker file to build through nuke (#15)
    
    Co-authored-by: sarimBasic <sarim.hussain@basichomeloan.com>
    
    * Feat:Impliment Auth and add docker file to build through nuke (#15) (#18)
    
    Co-authored-by: Sarim7714 <73176099+Sarim7714@users.noreply.github.com>
    Co-authored-by: sarimBasic <sarim.hussain@basichomeloan.com>
    
    * Fix: Apply Automation for creating and push Image
    
    * Fix:Conflict resolved
    
    ---------
    
    Co-authored-by: Sarim7714 <73176099+Sarim7714@users.noreply.github.com>
    Co-authored-by: sarimBasic <sarim.hussain@basichomeloan.com>

---
+++ shared/Auth0/Auth0/IAuth0Service.cs
@@ -0,0 +1,10 @@
+﻿using Microservice.Communication.GRPC.UserServiceProtos.Protos;
+
+namespace Auth0.Auth0
+{
+    public interface IAuth0Service
+    {
+        Task<bool> IsEmailVerifiedFromAuth0(string email);
+        Task<bool> ValidateJwtAsync(string token);
+    }
+}
