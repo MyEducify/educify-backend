1c2bc1d5c0c01cdb3977c334e907d57991120cf3
sarimBasic <sarim.hussain@basichomeloan.com>
2025-09-08 17:51:05 +0530

    fix:sabkuch

--- services/UserService/Controllers/ProfilesController.cs
+++ services/UserService/Controllers/ProfilesController.cs
@@ -22,6 +22,8 @@ namespace UserService.Controllers
             var profile = await _db.user.Where(p => p.Id == User_Id).FirstOrDefaultAsync();
             if (profile == null) return NotFound();
             return Ok(profile);
+
+
         }
     }
 }
